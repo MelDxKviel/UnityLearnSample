53be701c939b32def71ce51aeb960e8caf0e3cb6
odkdldo <c00lek1001@gmail.com>
2022-10-27 15:40:31 +0700

    ScalerModule

--- Assets/LearnMaterials 2/Scripts/ScalerModule.cs
+++ Assets/LearnMaterials 2/Scripts/ScalerModule.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1rdTEVSrCcYOjqTJcFCHj46RvnbdJhmQUb3gHMDhVftI/edit?usp=sharing")]
 public class ScalerModule : MonoBehaviour
 {
+    [SerializeField]
+    [Min(0)]
+    [Tooltip("from 0")]
     private Vector3 targetScale = new Vector3(2,2,2);
 
+    [SerializeField]
+    [Range(0.1f, 10)]
+    [Tooltip("from 0.1 to 10")]
     private float changeSpeed;
 
     private Vector3 defaultScale;
@@ -19,6 +25,8 @@ public class ScalerModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Run Script")]
+
     public void ActivateModule()
     {
         Vector3 target = toDefault ? defaultScale : targetScale;
@@ -27,17 +35,21 @@ public class ScalerModule : MonoBehaviour
         toDefault = !toDefault;
     }
 
+    [ContextMenu("ReturnToDefaultState")]
+
     public void ReturnToDefaultState()
     {
         toDefault = true;
         ActivateModule();
     }
 
+    [SerializeField]
     private IEnumerator ScaleCoroutine(Vector3 target)
     {
         Vector3 start = myTransform.lossyScale;
         float t = 0;
         while(t < 1)
+        
         {
             t += Time.deltaTime * changeSpeed;
             myTransform.localScale = Vector3.Lerp(start, target, t);
